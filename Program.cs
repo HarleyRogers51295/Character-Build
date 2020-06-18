3936592ee55e2fd2ae5f40b061d9d9aa84e72cbe
Harley Rogers <harleydev512@gmail.com>
2020-06-18 14:38:50 -0500

    Created Character Build

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace varPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Harley";
+            string lastName = "Rogers";
+            float damage = 59.7f;
+            decimal charisma = 100.59m;
+            double dexterity = 75.2;
+            bool male = true;
+            int age = 25;
+            decimal height = 6.1m;
+            char favLetter = 'h';
+
+
+
+            Console.WriteLine("Character Build");
+            Console.WriteLine($"Character Name: {firstName} {lastName}");
+            Console.WriteLine($"Character Stats: Damage:{damage}, Charisma:{charisma}, Dexterity: {dexterity} ");
+            Console.WriteLine($"Male: {male}, Age: {age}, Height: {height}");
+            Console.WriteLine($"His favorate letter in the alphabet is {favLetter}.");
+           
+        }
+    }
+}
